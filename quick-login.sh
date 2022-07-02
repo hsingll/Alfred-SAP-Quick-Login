00ad3e208a2f0bf23ba6a27da9157408d6e15ec2
SimonLing <lingleixing@outlook.com>
2022-07-02 13:52:26 +0800

    增加消息通知

--- quick-login.sh
+++ quick-login.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-set -e
+# set -e
 
-QUERY=$1
+QUERY=`echo $1`
 PATH=/usr/local/bin:$PATH
 
-prlctl exec $VM_UUID --current-user sapshcut.exe $QUERY
+RESULT=`prlctl exec $VM_UUID --current-user sapshcut.exe $QUERY 2>&1`
+
+echo $RESULT
+if [[ -n "$RESULT" ]] 
+then 
+    echo $RESULT
+else     
+    echo "已发送快捷登录命令，请至虚拟机中操作！"
+fi
